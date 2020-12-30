8599afd9866705a552b5e65d7ba6d52b2ec2a612
Sergio <Checcarelli>
2020-12-30 14:09:55 -0300

    agregado de detalles

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,14 +2,21 @@
     <div class="container">
         <div class="row">
             <div class="col-4">
-                <h1 class="text-white pt-3">Macnally</h1>
+                <h1 class="text-white pt-3 pb-5 text-center">
+                    Macnally
+                    <i class="bi-square" style="font-size: 3rem; color: white;"></i>    
+                </h1>
                 <ul class="pt-4">
-                    <li v-for="a in menus1" :key="a.id" class="text-light">
-                        {{a.menu}}
+                    <li v-for="a in menus1" :key="a.id" class="text-light pb-3">
+                        <button type="button" class="btn btn-secondary">
+                            <i :class= a.icon style="font-size: 1rem; color: white;"></i> {{a.menu}}
+                        </button>
                     </li>
-                    <p class="text-light pt-3"> USA API</p>
-                    <li v-for="a in menus2" :key="a.id" class="text-light">
-                        {{a.menu}}
+                    <p class="text-light pt-3 pb-3"> USA API</p>
+                    <li v-for="a in menus2" :key="a.id" class="text-light pb-3">
+                        <button type="button" class="btn btn-secondary">
+                            <i :class= a.icon style="font-size: 1rem; color: white;"></i> {{a.menu}}
+                        </button>
                     </li>
                 </ul>
             </div>
@@ -23,20 +30,38 @@ export default {
     data(){
         return{
             menus1: [
-                {menu: 'Companies'},
-                {menu: 'Investments News'}
+                {
+                    menu: 'Companies',
+                    icon: 'bi-layout-sidebar'
+                },
+                {
+                    menu: 'Investments News',
+                    icon:'bi-calendar4-week',
+                },
             ],
             menus2: [
-                {menu: 'Dashboards (Quandl)'},
-                {menu: 'Top Markets News'},
-                {menu: 'Stock details (iexcloud)'},
-                {menu: 'Fundamentals India (Quandl)'},
-                {menu: 'Robour global stock select (Quandl)'},
+                {
+                    menu: 'Dashboards (Quandl)',
+                    icon: 'bi-house',
+                },
+                {
+                    menu: 'Top Markets News',
+                    icon: 'bi-layout-sidebar'
+                },
+                {
+                    menu: 'Stock details (iexcloud)',
+                    icon: 'bi-shop-window',
+                    
+                },
+                {
+                    menu: 'Fundamentals India (Quandl)',
+                    icon: 'bi-x-diamond',
+                },
+                {
+                    menu: 'Robour global stock select (Quandl)',
+                    icon: 'bi-x-diamond',
+                },
             ],
-            items: [
-                { mensaje: 'Foo' },
-                { mensaje: 'Bar' }
-            ]
         }
     },
 }
@@ -51,6 +76,13 @@ export default {
         height: 100vh;
         background-color: #293042;
     }
-    
+    li {
+        list-style-type: none;
+    }
+    button {
+        border: none;
+        background: none;
+        box-shadow: none;
+    }
 </style>
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import './../node_modules/jquery/dist/jquery.min.js';
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
+import './../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+
+
 Vue.config.productionTip = false
 
 new Vue({
